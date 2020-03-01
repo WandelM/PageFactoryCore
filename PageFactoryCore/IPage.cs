556ab8e09907ab3fbf1d9d80194e25f25d69c5e3
Marcin Wandel <wandelmarcin@gmail.com>
2020-03-01 11:50:04 +0100

    Added IPage interface and PageFactory class

---
+++ PageFactoryCore/IPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PageFactoryCore
+{
+    /// <summary>
+    /// IPage interface, has to be used for every Page Object Model
+    /// </summary>
+    public interface IPage
+    {
+        /// <summary>
+        /// Instance of IWebDriver
+        /// </summary>
+        public IWebDriver Driver { get; set; }
+    }
+}
---
+++ PageFactoryCore/PageFactory.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using System.Reflection;
+
+namespace PageFactoryCore
+{
+    /// <summary>
+    /// Factory for pages
+    /// </summary>
+    class PageFactory
+    {
+        /// <summary>
+        /// Instance of IWebDriver
+        /// </summary>
+        public IWebDriver Driver { get; private set; }
+
+        /// <summary>
+        /// Primary constructor
+        /// </summary>
+        /// <param name="driver">Driver instance</param>
+        public PageFactory(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        /// <summary>
+        /// Returns new instance of Page Object Model
+        /// </summary>
+        /// <typeparam name="T">T has to be IPage type</typeparam>
+        /// <returns>New page object model</returns>
+        public T GetPage<T>() where T : IPage, new()
+        {
+            T pageObject = new T();
+
+            var driverProp = typeof(T).GetProperty(nameof(IPage.Driver));
+
+            driverProp.SetValue(driverProp, this.Driver);
+
+            return pageObject;
+        }
+    }
+}
